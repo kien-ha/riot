584d21208b62c87416b5d3bd33d02458abd31eda
Kien Ha <kienha9922@gmail.com>
2016-11-14 23:00:32 -0500

    Add Riot API project
    Add the initial setup of Riot API project for the game, League of
    Legends, onto github.
    
    Signed-off-by: Kien Ha <kienha9922@gmail.com>

---
+++ RiotConsts.py
@@ -0,0 +1,15 @@
+URL = {
+    'base': 'https://{proxy}.api.pvp.net/api/lol/{region}/{url}',
+    'summoner_by_name': 'v{version}/summoner/by-name/{names}',
+    'summoner_id': 'v{version}/summoner/{summonerids}',
+    'summoner_masteries': 'v{version}/summoner/{summonerids}/masteries'
+}
+
+API_VERSIONS = {
+    'summoner': '1.4'
+}
+
+REGIONS = {
+    'europe_west': 'euw',
+    'north_america': 'na'
+}
